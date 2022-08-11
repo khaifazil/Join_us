886a22c1a3705512d71fa457508bc4f034fc0819
Khai Fazil <khairal.fazil@gmail.com>
2022-08-11 20:23:47 +0800

    added - feature add fake users

--- app.js
+++ app.js
@@ -2,16 +2,52 @@ const { faker } = require('@faker-js/faker');
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'user',
-    password: 'password',
-    database: 'join_us'
+	host: 'localhost',
+	user: 'user',
+	password: 'password',
+	database: 'join_us'
 });
 
+// var q = 'SELECT COUNT(*) AS total FROM users';
+// connection.query(q, function (error, results, fields) {
+//     if (error) throw error;
+//     console.log(results[0].total);
+// });
 
-var q = 'SELECT 1';
-connection.query(q, function (error, results, fields) {
-    if (error) throw error;
-    console.log('The solution is: ', results);
+// var q = 'INSERT INTO users(email) values("wyatt_the_dog@gmail.com")';
+// connection.query(q, function (error, results, fields) {
+// 	if (error) throw error;
+// 	console.log(results);
+// });
+
+// var person = {
+// 	email: faker.internet.email(),
+// 	created_at: faker.date.past()
+// };
+
+// var end_result = connection.query('INSERT INTO users SET ?', person, function (err, result) {
+// 	if (err) throw err;
+// 	console.log(result);
+// });
+
+
+// connection.end();
+
+var data = [];
+for(var i = 0; i <500; i++){
+	data.push([
+		faker.internet.email(),
+		faker.date.past()
+	]);
+}
+
+console.log(data)
+
+var q = 'INSERT INTO users (email, created_at) VALUES ?';
+
+connection.query(q, [data], function (err, result) {
+	console.log(err);
+	console.log(result);
 });
 
+connection.end();
---
+++ schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    email VARCHAR(255) PRIMARY KEY,
+    created_at TIMESTAMP Default NOW()
+);
+
+INSERT INTO users (email) VALUES
+('Katie34@yahoo.com'), ('Tunde@gmail.com');
+
+INSERT INTO users (email, created_at) VALUES('dusty@gmail.com', '2021-08-13T17:28:33.589Z');
